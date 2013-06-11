b2d3c589dfe9c097515c737496ba97d83dfef1f2
Jason Goldstein <jason@betheshoe.com>
2013-06-11 11:49:57 -0400

    Changes to setup

--- setup.py
+++ setup.py
@@ -1,13 +1,29 @@
-from distutils.core import setup
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
 
 setup(
-    name='Django Autosave',
-    version="0.5.0",
+    name="Django Autosave",
+    version=__import__('autosave').__version__,
     author='Jason Goldstein',
     author_email='jason@betheshoe.com',
-    url='http://github.com/theatlantic/django-autosave',
+    url='https://github.com/theatlantic/django-autosave',
     packages=['autosave', ],
     package_data={ 'autosave': ['static/*',] },
-    description='Generic autsoave for the Django Admin.',
-    long_description=open('README.markdown').read(),
+    description='Generic autosave for the Django Admin.',
+    long_description=open('README.md').read(),
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    include_package_data=True,
+    zip_safe=False,
 )
